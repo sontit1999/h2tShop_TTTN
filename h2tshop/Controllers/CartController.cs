c97fe96d29f8a5e0285c2910c9e470a11c341669
sontit99 <sonkoii1995@gmail.com>
2021-02-03 21:57:55 +0700

    done gio hang

--- h2tshop/Controllers/CartController.cs
+++ h2tshop/Controllers/CartController.cs
@@ -13,6 +13,7 @@ namespace h2tshop.Controllers
         // GET: Cart
         public ActionResult Index()
         {
+            int tongtien = 0;
             List<CartItemDTO> listCart = new List<CartItemDTO>();
             var numberItem = 0;
             var cart = Session["cart"];//get key cart
@@ -21,6 +22,11 @@ namespace h2tshop.Controllers
                 listCart = JsonConvert.DeserializeObject<List<CartItemDTO>>(cart.ToString());
                 numberItem = listCart.Count;
             }
+            foreach (var item in listCart)
+            {
+                tongtien += (Convert.ToInt32(item.Gia) * Convert.ToInt32(item.SoLuong));
+            }
+            ViewBag.tongtien = string.Format("{0:#,##0}", tongtien);
             ViewBag.listCart = listCart;
             ViewBag.numberItem = numberItem;
             return View();
@@ -65,8 +71,9 @@ namespace h2tshop.Controllers
             return RedirectToAction("Index","Home");
         }
         [HttpPost]
-        public string updateCart(CartUpdateDTO cartUpdate)
+        public int updateCart(CartUpdateDTO cartUpdate)
         {
+            int tongtien = 0;
             var cart = Session["cart"];//get key cart
             if (cart == null)
             {
@@ -86,12 +93,45 @@ namespace h2tshop.Controllers
                         break;
                     }
                 }
+                foreach(var item in listCart)
+                {
+                    tongtien += (Convert.ToInt32(item.Gia) * Convert.ToInt32(item.SoLuong));
+                }
+                string jsonCart = JsonConvert.SerializeObject(listCart);
+                Session["cart"] = jsonCart;
                
+            }
+            return tongtien;
+        }
+        [HttpPost]
+        public int deleteCart(CartUpdateDTO cartUpdate)
+        {
+          
+            var cart = Session["cart"];//get key cart
+            if (cart == null)
+            {
+                List<CartItemDTO> listCart = new List<CartItemDTO>();
+                string jsonCart = JsonConvert.SerializeObject(listCart);
+                Session["cart"] = jsonCart;
+            }
+            else
+            {
+                List<CartItemDTO> listCart = JsonConvert.DeserializeObject<List<CartItemDTO>>(cart.ToString());
+
+                for (int i = 0; i < listCart.Count; i++)
+                {
+                    if (listCart[i].MaSanPham == cartUpdate.MaSanPham)
+                    {
+                        listCart.Remove(listCart[i]);
+                        break;
+                    }
+                }
+                
                 string jsonCart = JsonConvert.SerializeObject(listCart);
                 Session["cart"] = jsonCart;
 
             }
-            return "true";
+            return 1;
         }
     }
 }
